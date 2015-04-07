b307e7fd32d62797f74f2b63e01f9f3c8250c251
Michael Misciagna <mmisciagna@google.com>
2015-04-07 15:12:28 -0700

    add detailed info to Design page

--- app/design/DesignFactory.js
+++ app/design/DesignFactory.js
@@ -3,36 +3,33 @@ design.factory('Designs', function() {
 
   designs.items = [
     {
-      'title': 'AngularDart.org',
-      'thumb': [
+      title: 'AngularDart.org',
+      link: 'http://www.michaelmisciagna.com',
+      thumb: [
         {
-          'src': 'thumb_angularDart.png',
-          'alt': 'AngularDart.org thumbnail'
+          src: 'thumb_angularDart.png',
+          alt: 'AngularDart.org thumbnail'
         }
       ],
+      role: 'UI Designer & Prototyper',
+      goal: 'Design, develop and launch within in 2 weeks.',
+      solution: 'A simple, easy-to-navigate site design that reflected the red/black of AngularJS rather than the  blue/green of Dart.'
     },
 
 
     {
-      'title': 'gAnswers',
-      'thumb': [
+      title: 'gAnswers',
+      link: 'http://www.google.com',
+      thumb: [
         {
-          'src': 'thumb_gAnswers.svg',
-          'alt': 'gAnswers thumbnail'
+          src: 'thumb_gAnswers.svg',
+          alt: 'gAnswers thumbnail'
         }
       ],
-    },
-    
-
-    {
-      'title': 'Payments Doc Center',
-      'thumb': [
-        {
-          'src': 'thumb_angularDart.png',
-          'alt': 'Payments Doc Center thumbnail'
-        }
-      ],
-    },
+      role: 'Visual / UI designer & protoyper',
+      goal: 'Design and build a Chrome extension for gTechers to post canned response-style answers to common questions, and ensure that it is searchable by all gTechers across all products.',
+      solution: 'Created a website and prototype styled after Google’s Material design.'
+    }
   ];
 
   return designs;
