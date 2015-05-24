bb118f41ebb628c3f419d6a7bd21502e311be125
Michael Misciagna <mmisciagna@google.com>
2015-05-23 23:26:26 -0700

    change accent color to a neon lime; change name of blueGray to primary and teal to accent

--- app/code/CodeFactory.js
+++ app/code/CodeFactory.js
@@ -16,7 +16,7 @@ codeGallery.factory('Code', function($http) {
       poster: 'placeholder_2_poster.png',
       title: 'Title',
       link: 'http://www.google.com',
-      youtube: '<iframe src="//www.youtube.com/embed/Os50zJZ9oYU?&controls=2&modestbranding=1&rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>',
+      youtube: '<iframe src="//youtube.com/embed/-Rlz_2NSmhA?&controls=2&modestbranding=1&rel=0&showinfo=0" frameborder="0" allowfullscreen></iframe>',
       role: 'My role',
       goal: 'The goal',
       solution: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
